000cde4c4a581ead1d34b3c438d15e72d465266a
Nickolas <nickolasiaquinta@gmail.com>
2021-07-03 13:56:20 -0400

    Replacing xfce-temrinal with qterminal in config.def.h

--- config.def.h
+++ config.def.h
@@ -57,7 +57,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "xfce4-terminal", NULL };
+static const char *termcmd[]  = { "qterminal", NULL };
 static const char *volup[] = { "amixer", "-q", "set", "Master", "2%+", "unmute", NULL };
 static const char *voldn[] = { "amixer", "-q", "set", "Master", "2%-", "unmute", NULL };
 
