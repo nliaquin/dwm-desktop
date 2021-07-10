7b9c2790ffa6a17c25bf8829b4f562b1d1aacae1
Nickolas <nickolasiaquinta@gmail.com>
2021-07-10 12:49:12 -0400

    Changing font; Changing volume controls

--- config.def.h
+++ config.def.h
@@ -6,8 +6,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "arial:size=12" };
+static const char dmenufont[]       = "arial:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -16,7 +16,7 @@ static const char col_red[]         = "#a30306";
 static const char *colors[][3]      = {
     /*               fg         bg         border   */
     [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-    [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+    [SchemeSel]  = { col_gray4, col_red,  col_red  },
 };
 
 /* tagging */
@@ -56,10 +56,10 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_red, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "qterminal", NULL };
-static const char *volup[] = { "amixer", "-q", "set", "Master", "2%+", "unmute", NULL };
-static const char *voldn[] = { "amixer", "-q", "set", "Master", "2%-", "unmute", NULL };
+static const char *volup[] = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%", NULL };
+static const char *voldn[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%", NULL };
 
 static Key keys[] = {
     /* modifier, key, function, argument */
