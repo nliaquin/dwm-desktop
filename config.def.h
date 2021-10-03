75e2e09bc62efb4976a0b39eca8ada8ead2d6ed4
Nickolas <nickolasiaquinta@gmail.com>
2021-10-03 16:48:03 -0400

    Rewrote config.def.h

--- config.def.h
+++ config.def.h
@@ -1,67 +1,83 @@
-/* See LICENSE file for copyright and license details. */
 #include <X11/XF86keysym.h>
 
-/* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "arial:size=12" };
-static const char dmenufont[]       = "arial:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_red[]         = "#a30306";
-static const char *colors[][3]      = {
-    /*               fg         bg         border   */
-    [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-    [SchemeSel]  = { col_gray4, col_red,  col_red  },
+/* Theme */
+// border pixel of windows
+static const unsigned int borderpx = 1;
+
+// snap pixel
+static const unsigned int snap = 32;
+
+// bar visibility (0 = invisible, 1 = visible
+static const int showbar = 1;
+
+// bar location (0 = bottom, 1 = top)
+static const int topbar = 1;
+
+// bar font
+static const char *fonts[] = { "arial:size=12" };
+
+// dmenu font
+static const char dmenufont[] = "arial:size=12";
+
+// colors
+static const char col_gray1[] = "#222222";
+static const char col_gray2[] = "#444444";
+static const char col_gray3[] = "#bbbbbb";
+static const char col_gray4[] = "#eeeeee";
+static const char col_red[] = "#a30306";
+static const char *colors[][3] = {
+    // fg, bg, border
+
+    [SchemeNorm] = { colr_gray3, col_gray1, col_gray2 },
+    [SchemeSel] = { col_gray4, col_red, col_red },
 };
 
-/* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "term", "ssh" };
 
+/* Tags */
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const Rule rules[] = {
-    /* xprop(1):
-     *	WM_CLASS(STRING) = instance, class
-     *	WM_NAME(STRING) = title
-     */
-    /* class      instance    title       tags mask     isfloating   monitor */
-    { "mpv",     NULL,       NULL,       0,            1,           -1 },
+    // class, instance, title, tag mask, isfloating, monitor
+
+    { "mpv", NULL, NULL, 0, 1, -1 },
 };
 
-/* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+
+/* Layouts */
+// factor of master area size
+static const float mfact = 0.55;
+
+// number of clients in master area
+static const int nmaster = 1;
+
+// 1 means respect hints in tiled resizals
+static const int resizehints = 1;
 
 static const Layout layouts[] = {
-    /* symbol     arrange function */
-    { "[]=",      tile },    /* first entry is default */
-    { "><>",      NULL },    /* no layout function means floating behavior */
-    { "[M]",      monocle },
+    // symbol, arrange function
+
+    { "[ ]", tile },
+    { "> <", NULL }, //means floating
+    { "[M]", monocle },
 };
 
-/* key definitions */
-#define MODKEY Mod4Mask
-#define TAGKEYS(KEY,TAG) \
-    { MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-    { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-    { MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-    { MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
-/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+/* Key Definitions */
+#define MODKEY Mod4Mask
+#define TAGKEYS(KEY, TAG) \
+    { MODKEY, KEY, view, {.ui = 1 << TAG} }, \
+    { MODKEY|ControlMask, KEY, toggleview, {.ui = 1 << TAG} }, \
+    { MODKEY|ShiftMask, KEY, tag, {.ui = 1 << TAG} }, \
+    { MODKEY|ControlMask|ShiftMask, KEY, toggletag, {.ui = 1 << TAG} },
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/bash", "-c", cmd, NULL } }
 
-/* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_red, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "qterminal", NULL };
-static const char *volup[] = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%", NULL };
-static const char *voldn[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%", NULL };
-static const char *prntscrn[] = { "/usr/bin/scrot", "%Y-%m-%d_%H-%M-%S_$wx$h.png", "-e 'mv $f /home/$USER/screenshots/'", NULL };
 
+/* Commands */
+static char dmenumon[2] = "0";
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_red, "-sf", col_gray4, NULL };
+static const char *termcmd[] = { "qterminal", NULL };
+static const char *volup[] = { "/usr/bin/amixer", "set", "Master", "2%+", NULL };
+static const char *voldn[] = { "/usr/bin/amixer", "set", "Master", "2%-", NULL };
+static const char *prntscrn[] = { "/usr/bin/scrot", "%Y-%m-%d_%H-%M-%S_$wx$h.png", "-e 'mv $f /home/$USER/screenshots/'", NULL };
 static Key keys[] = {
     /* modifier, key, function, argument */
     { MODKEY, XK_Return, spawn, {.v = dmenucmd } },
@@ -102,8 +118,8 @@ static Key keys[] = {
     { MODKEY|ShiftMask, XK_q, quit, {0} },
 };
 
-/* button definitions */
-/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+
+/* Button Definitions */
 static Button buttons[] = {
     /* click, event mask, button, function, argument */
     { ClkLtSymbol, 0, Button1, setlayout, {0} },
